ae8e908a9a58bd0de92ef20117c84193029790e7
Akshay Garg <ak561005@dal.ca>
2021-05-19 01:49:59 -0300

    Changed server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static('/frontend/build'));
+app.use(express.static('./frontend/build'));
 
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
