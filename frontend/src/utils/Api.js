b3ac86884b2bfdca18fb631cd99c5b9546e74c70
Jay Patel <jy680475@dal.ca>
2021-07-18 11:56:43 -0300

    Edit gitignore

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -2,7 +2,8 @@ const axios = require("axios");
 
 const port = process.env.PORT || 8080;
 
-const backEndUrl = process.env.BACKEND_HOSTNAME || 'dalffins.herokuapp.com';
+const backEndUrl =
+  process.env.REACT_APP_BACKEND_HOSTNAME || "dalffins.herokuapp.com";
 
 const domain = `https://${backEndUrl}`;
 
@@ -22,30 +23,30 @@ export function saveTicketApi(data) {
   return axiosInstance.post(`/help/tickets`, data);
 }
 
-export function saveUser(data,header) {
-  return axiosInstance.post(`/user/signUp`, data,header);
+export function saveUser(data, header) {
+  return axiosInstance.post(`/user/signUp`, data, header);
 }
 
-export function validateLogin(data,header){
-  return axiosInstance.post(`/user/login`, data,header);
+export function validateLogin(data, header) {
+  return axiosInstance.post(`/user/login`, data, header);
 }
 
-export function fetchUserProfile(userId,header) {
-  return axiosInstance.get(`/user/userProfile/${userId}`,header)
+export function fetchUserProfile(userId, header) {
+  return axiosInstance.get(`/user/userProfile/${userId}`, header);
 }
 
-export function updateUserProfile(userId,data,header){
-  return axiosInstance.put(`/user/updateProfile/${userId}`,data, header)
+export function updateUserProfile(userId, data, header) {
+  return axiosInstance.put(`/user/updateProfile/${userId}`, data, header);
 }
 
-export function deleteUserProfile(userId){
-  return axiosInstance.delete(`/user/deleteProfile/${userId}`)
+export function deleteUserProfile(userId) {
+  return axiosInstance.delete(`/user/deleteProfile/${userId}`);
 }
 
-export function emailCheck(data,header) {
-  return axiosInstance.post(`/user/emailCheck`,data,header)
+export function emailCheck(data, header) {
+  return axiosInstance.post(`/user/emailCheck`, data, header);
 }
 
-export function resetPassword(data,header){
-  return axiosInstance.put(`/user/resetPassword`,data,header)
+export function resetPassword(data, header) {
+  return axiosInstance.put(`/user/resetPassword`, data, header);
 }
