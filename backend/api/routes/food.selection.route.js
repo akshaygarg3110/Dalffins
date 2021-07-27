7853a907c57e216144e4a6a67d58920392db54f0
Akshay Garg <ak561005@dal.ca>
2021-07-27 01:45:28 -0300

    Committing backend

--- backend/api/routes/food.selection.route.js
+++ backend/api/routes/food.selection.route.js
@@ -9,4 +9,6 @@ const foodSelectionController = require("../controller/food.selection.controller
 // Matches the input url with the uri and call the getKitchenByOwnerId function of foodSelectionController.
 router.get("/:id", foodSelectionController.getKitchenByOwnerId);
 
+router.get("/vendors/all", foodSelectionController.getVendors);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //Author: Divyashree Bangalore Subbaraya (B00875916)
 const express = require("express");
 
-const http = require('http')
+const http = require("http");
 
-const app = require('./backend/app');
+const app = require("./backend/app");
 
 const port = process.env.PORT || 8080;
 
-const backEndUrl = process.env.BACKEND_HOSTNAME || 'localhost';
+const backEndUrl = process.env.BACKEND_HOSTNAME || "localhost";
 
 const server = http.createServer(app);
 
@@ -17,14 +17,12 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static('./frontend/build'));
+app.use(express.static("./frontend/build"));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
 server.listen(port, () => {
-    console.log(`server listening at http://${backEndUrl}:${port}`);
+  console.log(`server listening at http://${backEndUrl}:${port}`);
 });
-
-
